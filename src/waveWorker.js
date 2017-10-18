7ed80d7549b5e0d7ffb06aa5b9a97c84d884c902
rochars <rocha.rafaelsilva@gmail.com>
2017-10-18 01:24:45 -0200

    64-bit audio

--- src/waveWorker.js
+++ src/waveWorker.js
@@ -36,8 +36,8 @@ var WavePCM = function( config ){
     throw new Error("wavSampleRate value is required to record. NOTE: Audio is not resampled!");
   }
 
-  if ( ![8, 16, 24, 32].includes( config['wavBitDepth'] ) ) {
-    throw new Error("Only 8, 16, 24 and 32 bits per sample are supported");
+  if ( ![8, 16, 24, 32, 64].includes( config['wavBitDepth'] ) ) {
+    throw new Error("Only 8, 16, 24, 32 and 64 bits per sample are supported");
   }
 
   this.bitDepth = config['wavBitDepth'];
@@ -69,6 +69,19 @@ WavePCM.prototype.record = function( buffers ){
 
       // bit reduce and convert to uInt
       switch ( this.bytesPerSample ) {
+        case 8:
+          // 64-bit is in the -1.0 - 1.0 range
+          sample = this.splitFloat64(sample);
+          reducedData[ outputIndex ] = sample[1];
+          reducedData[ outputIndex + 1 ] = sample[1] >> 8;
+          reducedData[ outputIndex + 2 ] = sample[1] >> 16;
+          reducedData[ outputIndex + 3 ] = sample[1] >> 24;
+          reducedData[ outputIndex + 4 ] = sample[0] >> 32;
+          reducedData[ outputIndex + 5 ] = sample[0] >> 40;
+          reducedData[ outputIndex + 6 ] = sample[0] >> 48;
+          reducedData[ outputIndex + 7 ] = sample[0] >> 56;
+          break;
+
         case 4:
           sample = sample * 2147483648;
           reducedData[ outputIndex ] = sample;
@@ -95,12 +108,47 @@ WavePCM.prototype.record = function( buffers ){
           break;
 
         default:
-          throw new Error("Only 8, 16, 24 and 32 bits per sample are supported");
+          throw new Error("Only 8, 16, 24, 32 and 64 bits per sample are supported");
       }
     }
   }
 
   this.recordedBuffers.push( reducedData );
+};
+
+WavePCM.prototype.splitFloat64 = function(floatValue) {
+        var hiWord = 0;
+        var loWord = 0;
+        switch (floatValue) {
+            case +0.0:
+                hiWord = 0x40000000;
+                break;
+            case -0.0:
+                hiWord = 0xC0000000;
+                break;
+            default:
+                if (floatValue <= -0.0) {
+                    hiWord = 0x80000000;
+                    floatValue = -floatValue;
+                }
+                var exponent = Math.floor(
+                    Math.log(floatValue) / Math.log(2));
+                var significand = Math.floor(
+                    (floatValue / Math.pow(2, exponent)) * Math.pow(2, 52));
+                loWord = significand & 0xFFFFFFFF;
+                significand /= Math.pow(2, 32);
+                exponent += 1023;
+                if (exponent >= 0x7FF) {
+                    exponent = 0x7FF;
+                    significand = 0;
+                } else if (exponent < 0) {
+                    exponent = 0;
+                }
+                hiWord = hiWord | (exponent << 20);
+                hiWord = hiWord | (significand & ~(-1 << 20));
+                break;
+        }
+        return [hiWord, loWord];
 };
 
 WavePCM.prototype.requestData = function(){
@@ -115,7 +163,7 @@ WavePCM.prototype.requestData = function(){
   view.setUint32( 8, 1463899717, false ); // RIFF type 'WAVE'
   view.setUint32( 12, 1718449184, false ); // format chunk identifier 'fmt '
   view.setUint32( 16, 16, true ); // format chunk length
-  view.setUint16( 20, 1, true ); // sample format (raw)
+  view.setUint16( 20, this.bitDepth === 64 ? 3 : 1, true ); // sample format (1 for PCM raw, 3 for IEEE)
   view.setUint16( 22, this.numberOfChannels, true ); // channel count
   view.setUint32( 24, this.sampleRate, true ); // sample rate
   view.setUint32( 28, this.sampleRate * this.bytesPerSample * this.numberOfChannels, true ); // byte rate (sample rate * block align)
